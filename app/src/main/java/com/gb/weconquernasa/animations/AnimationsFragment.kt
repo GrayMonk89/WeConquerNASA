ad31f32535380892c15a5e5b8158cf53e58b32ec
graymonk89 <graymonk89@yandex.ru>
2022-06-21 13:22:07 +0300

    Урок 5 потрогал TransitionManager

--- app/src/main/java/com/gb/weconquernasa/animations/AnimationsFragment.kt
+++ app/src/main/java/com/gb/weconquernasa/animations/AnimationsFragment.kt
@@ -5,18 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.transition.ChangeBounds
+import androidx.transition.Fade
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import com.gb.weconquernasa.R
 import com.gb.weconquernasa.databinding.FragmentAnimationsBinding
 
 
 class AnimationsFragment : Fragment() {
-    private var _binding: FragmentAnimationsBinding? =null
-    private val binding: FragmentAnimationsBinding
-    get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    var isOpen: Boolean = false
+
+    private var _binding: FragmentAnimationsBinding? = null
+    private val binding: FragmentAnimationsBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +28,39 @@ class AnimationsFragment : Fragment() {
         _binding = FragmentAnimationsBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setButtonAnimationListener()
+
+    }
+
+    private fun setButtonAnimationListener() {
+        binding.buttonAnimation.setOnClickListener {
+            isOpen = !isOpen
+
+            initTransitionManager()
+
+            binding.textForAnimation.visibility = if (isOpen) {
+                View.VISIBLE
+            } else {
+                View.INVISIBLE
+            }
+        }
+    }
+
+    private fun initTransitionManager() {
+        val transitionFade = Fade()
+        transitionFade.duration = 1500
+        val transitionChangeBounds = ChangeBounds()
+        transitionChangeBounds.duration = 2500
+        val transitionSet = TransitionSet()
+        transitionSet.addTransition(transitionFade)
+        transitionSet.addTransition(transitionChangeBounds)
+        TransitionManager.beginDelayedTransition(binding.transitionsContainer, transitionSet)
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
