855423f2d13feacbdec7394bb47a831bc6eca018
Gray Monk <graymonk89@yandex.ru>
2022-06-22 17:14:37 +0300

    урок 5 потрогал ConstraintSet через макеты

--- app/src/main/java/com/gb/weconquernasa/animations/AnimationsFragment.kt
+++ app/src/main/java/com/gb/weconquernasa/animations/AnimationsFragment.kt
@@ -6,6 +6,7 @@ import android.transition.TransitionManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnticipateOvershootInterpolator
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.fragment.app.Fragment
 import com.gb.weconquernasa.R
@@ -13,6 +14,9 @@ import com.gb.weconquernasa.databinding.FragmentAnimationsBonusStartBinding
 
 
 class AnimationsFragment : Fragment() {
+
+    private val duration: Long = 1000
+    var isOpen: Boolean = false
 
     private var _binding: FragmentAnimationsBonusStartBinding? = null
     private val binding: FragmentAnimationsBonusStartBinding
@@ -30,11 +34,23 @@ class AnimationsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
-        binding.backgroundImage.setOnClickListener {
-            val constraintSet = ConstraintSet()//.clone(binding.constraintContainer)
+        initShowPhotoDetails()
+    }
 
-            TransitionManager.beginDelayedTransition(binding.constraintContainer, ChangeBounds())
-            constraintSet.clone(requireContext(), R.layout.fragment_animations_bonus_end)
+    private fun initShowPhotoDetails() {
+        binding.backgroundImage.setOnClickListener {
+
+            val constraintSet = ConstraintSet()
+            val transition = ChangeBounds()
+            transition.interpolator = AnticipateOvershootInterpolator(20f)
+            transition.duration = duration
+            TransitionManager.beginDelayedTransition(binding.constraintContainer,transition)
+            isOpen = !isOpen
+            if (isOpen) {
+                constraintSet.clone(requireContext(), R.layout.fragment_animations_bonus_end)
+            } else {
+                constraintSet.clone(requireContext(), R.layout.fragment_animations_bonus_start)
+            }
             constraintSet.applyTo(binding.constraintContainer)
         }
     }
